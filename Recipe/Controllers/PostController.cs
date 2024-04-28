68d33b9483503716b06d4582007306ffb6d29007
tanvirhossainapon <tanvirhossainapon@gmail.com>
2024-04-29 01:31:37 +0600

    Recipe Share Initial Code

---
+++ Recipe/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Recipe.Controllers
+{
+    public class PostController : ApiController
+    {
+        [HttpGet]
+        [Route("api/posts")]
+        public HttpResponseMessage Posts()
+        {
+            try
+            {
+                var data = PostService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+    }
+}
